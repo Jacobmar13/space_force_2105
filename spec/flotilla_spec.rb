11af43c6edb841e485987df07130f5df04b0b5d2
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-03 09:51:07 -0600

    Make recommend personnel test

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -64,5 +64,35 @@ RSpec.describe 'Flotilla' do
 
       expect(seventh_flotilla.personnel).to eq([kathy, polly, rover, sampson])
     end
+
+    it 'can recommend personnel' do
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      daedalus.add_requirement({astrophysics: 6})
+      daedalus.add_requirement({quantum_mechanics: 3})
+      seventh_flotilla.add_ship(daedalus)
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+      polly = Person.new('Polly Parker', 8)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+      rover = Person.new('Rover Henriette', 1)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+      sampson = Person.new('Sampson Edwards', 7)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
+      seventh_flotilla.add_personnel(kathy)
+      seventh_flotilla.add_personnel(polly)
+      seventh_flotilla.add_personnel(rover)
+      seventh_flotilla.add_personnel(sampson)
+      odyssey = Spacecraft.new({name: 'Odyssey', fuel: 300})
+      odyssey.add_requirement({operations: 6})
+      odyssey.add_requirement({maintenance: 3})
+
+      expect(seventh_flotilla.recommend_personnel(daedalus)).to eq([kathy, sampson])
+      expect(seventh_flotilla.recommend_personnel(odyssey)).to eq([polly])
+    end
   end
 end
