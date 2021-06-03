147d233146df3d5960271ef644d9db3a45bdb7c1
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-03 09:22:52 -0600

    Add tests to person spec file

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,5 +8,30 @@ RSpec.describe 'Person' do
 
       expect(kathy).to be_a(Person)
     end
+
+    it 'has a name' do
+      kathy = Person.new('Kathy Chan', 10)
+
+      expect(kathy.name).to eq('Kathy Chan')
+    end
+
+    it 'has experience' do
+      kathy = Person.new('Kathy Chan', 10)
+
+      expect(kathy.experience).to eq(10)
+    end
+    it 'can check for specialties' do
+      kathy = Person.new('Kathy Chan', 10)
+
+      expect(kathy.specialties).to eq([])
+    end
+
+    it 'can add specialties' do
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+    end
   end
 end
